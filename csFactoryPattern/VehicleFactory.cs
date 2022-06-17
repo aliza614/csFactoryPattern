96c20493486ff94ae4863063c78fd704772e191e
Aliza OConnor <alizaoconnor1@yahoo.com>
2022-06-17 12:37:12 -0500

    initial commit

---
+++ csFactoryPattern/VehicleFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csFactoryPattern
+{
+    public class VehicleFactory
+    {
+        public static IVehicle GetVehicle(int tires)
+        {
+            switch (tires)
+            {
+                case 2:
+                    return new Motorcycle();
+                    break;
+                case 4:
+                    return new Car();
+                    break;
+                default:
+                    return new Car();
+                    break;
+            }
+        }
+    }
+}
